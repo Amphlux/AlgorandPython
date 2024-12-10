50e25f02f5d5b4e160ad1bb1a4e4a8a8c553e75d
Amphlux <115769719+Amphlux@users.noreply.github.com>
2024-12-10 03:35:08 -0600

    Update VanityAddressWithProgress.py

--- VanityAddressWithProgress.py
+++ VanityAddressWithProgress.py
@@ -1,14 +1,6 @@
-#define a function to generate Algorand wallet accounts, and search them for strings
-#useful for creating a vanity algorand address
-#you can search anywhere in the address, the front or the back of the address
-#inspired by https://algovanity.com/
-#note: the larger the search space, the more addresses that need to be generated to search
-#searching for terms 5 letters long or longer will require at least millions of addresses to be generated
-
-#import the relevant packages from AlgoSDK
 from algosdk import account, mnemonic
+from tqdm import tqdm
 
-#define non-multi search function
 def generate_algorand_keypair_search(searchterm, num, pos):
     private_key, address = account.generate_account()
     if pos == "A":
@@ -26,7 +18,6 @@ def generate_algorand_keypair_search(searchterm, num, pos):
     else: 
         print("Pick F, A or B!")
 
-#functions for multi-search        
 def generate_algorand_keypair():
     private_key, address = account.generate_account()
     return private_key, address
@@ -39,36 +30,38 @@ def search_back(bterm, bnum, faddress):
     if faddress[-bnum:] == bterm:
         print("My address: \n{}".format(faddress))
         print("My passphrase: \n{}".format(mnemonic.from_private_key(private_key)))
-        
-        
-#request number of keypairs to generate
+
+# Request number of keypairs to generate
 y = int(input("Please enter the number of Algorand keypairs to generate: \n"))
 
-#request multi_search setup
+# Request multi_search setup
 m = str.upper(input("Do you want to search the front + back? (Y)es or (N)o \n"))
 if m == "Y":
     multi = True
 else:
     multi = False
-    
-#multi-search allows user to search for keywords at both the front and the back of the address
+
+# Multi-search allows user to search for keywords at both the front and the back of the address
 if multi == True:
-    #request parameters
+    # Request parameters
     fnum = int(input("How many digits to search within the front of the address: \n"))
     fterm = str(input(f"Type {fnum} character(s): \n"))
     bnum = int(input("How many digits to search within the back of the address: \n"))
     bterm = str(input(f"Type {bnum} character(s): \n"))
-    for i in range(y):
+    
+    print("\nSearching for addresses... Press Ctrl+C to stop")
+    for i in tqdm(range(y), desc="Generating addresses", unit="addr"):
         private_key, address = generate_algorand_keypair()
         faddress = search_front(fterm, fnum, address)
         if faddress != None:
-            search_back(bterm, bnum, faddress) 
-            
-#normal search function to search the (F)ront, (B)ack or (A)nywhere within an Algorand address            
+            search_back(bterm, bnum, faddress)
+
 else:
-    #request parameters
+    # Request parameters
     num = int(input("How many digits to search within the address: \n"))
     searchterm = str(input(f"Type {num} character(s): \n"))
     pos = str.upper(input("Do you want to search the (F)ront, (B)ack or (A)nywhere in the address: \n"))
-    for i in range(y):
-        generate_algorand_keypair_search(searchterm, num, pos)            
+    
+    print("\nSearching for addresses... Press Ctrl+C to stop")
+    for i in tqdm(range(y), desc="Generating addresses", unit="addr"):
+        generate_algorand_keypair_search(searchterm, num, pos)
